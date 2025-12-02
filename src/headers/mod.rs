3e7288c6c09d12adc2a85fcd851ba1c720054173
Manuel Pelloni <manuel.pelloni@m4ss.net>
2025-12-02 14:19:06 +0000

    fix: `Display` implementation of `OrderedRange`

--- src/headers/mod.rs
+++ src/headers/mod.rs
@@ -91,7 +91,7 @@ impl OrderedRange {
 
 impl Display for OrderedRange {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{UNIT}={}-{}", self.start(), self.end())
+        write!(f, "{}-{}", self.start(), self.end())
     }
 }
 
